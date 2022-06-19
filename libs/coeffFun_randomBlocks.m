79b402c7ef3077d222b6a421244a19546c80d34d
Ricu <vl1102@web.de>
2022-06-19 20:27:11 +0200

    VALENTIN: Dateien verschoben

--- libs/coeffFun_randomBlocks.m
+++ libs/coeffFun_randomBlocks.m
@@ -1,19 +1,21 @@
-function [markedVertices] = coeffFun_randomBlocks(x,y,n_blocks,h)
+function [markedVertices] = coeffFun_randomBlocks(x,y,N,n,n_blocks)
 assert(all(size(x) == size(y)),'Die Vektoren x und y haben unterschiedliche Groesse')
-numCor = sqrt(numVert);
-corVec = vert(1:numCor,2);
 
-markedVertices = false(size(x),1); %initialisiere mit logical false fuer Punkt
+numCoord = N*n+1;
+coordVec = linspace(0,1,numCoord)';
+h = 1/(N*n);
+
+markedVertices = false(size(x)); %initialisiere mit logical false fuer Punkt
 width = 1:5;
 height = 1:5;
 for current_block = 1:n_blocks
-    randx = randi([1 numCor]);
-    randy = randi([1 numCor]);
+    randx = randi([1 numCoord]);
+    randy = randi([1 numCoord]);
     randwidth = randi([1 length(width)]);
     randheight = randi([1 length(height)]);
 
-    vertx = corVec(randx);
-    verty = corVec(randy);
+    vertx = coordVec(randx);
+    verty = coordVec(randy);
     block_width = width(randwidth);
     block_height = height(randheight);
 
