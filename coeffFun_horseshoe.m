a36e5985c571b3a7d8f80c93af6cd3d4906772b0
proth22 <103263791+proth22@users.noreply.github.com>
2022-06-23 17:11:54 +0200

    Paula: main Aufgabe2 angepasst fuer coeffFun_horseshoe + coeffFun-horseshoe angepasst
    Es läuft jetzt alles gut dank Toleranz von 10^(-6) und sieht aus wie Schaubild i)

--- coeffFun_horseshoe.m
+++ coeffFun_horseshoe.m
@@ -1,8 +1,12 @@
-function [markedVertices] = coeffFun_horseshoe(vert,x,y,N,n,yStripeLim,position,width,hight)
+function [elements] = coeffFun_horseshoe(tri,x,y,N,n,yStripeLim,position,width,hight)
 assert(all(size(x) == size(y)),'Die Vektoren x und y haben unterschiedliche Groesse')
 SD_size = 1/N;
 h = 1/(N*n);
+numTri = size(tri,1);
+elements = false(numTri,1); % Logischer Vektor, welche Elemente hoeheren Koeffizienten haben
 
+tol = 10^(-6); %Toleranz gegen Rundungsfehler
+ 
 %Erstellen der vertikalen Streifen
 indySt = (yStripeLim(1) <= y) & (y <= yStripeLim(2));  %Logischer Vektor, welche Knoten im Streifen liegen
 indxSt = false(size(x));    %initialisiere mit logical false fuer x-Koordinate
@@ -11,30 +15,51 @@ for j = 0:N-1
     bool2 = SD_size*(j+0.5)+h*(1+position+width);
     indxSt = indxSt | ((bool1 <= x) & (x <= bool2));
 end
-indSt = indySt & indxSt;  %Logischer Vektor, welche Knoten in den Streifen liegen
-
+indSt = indySt & indxSt;
+vertNum = find(indSt); % Knotennummern der Knoten mit hoehem Koeffizienten
+elements(sum(ismember(tri,vertNum),2)==3) = true;
 
 %Erstelle Hufeisen
 indHuf = false(size(x)); %Logischer Vektor, welche Knoten im Hufeisen liegen
                          %initialisiere mit logical false fuer x- und y-Koordinate
 for j = 0:N-1   %iteriere ueber TG in x-Richtung
-    bool3 = SD_size*j + h*position;
     for i = 1:N-1   %iteriere ueber y-Koordinaten
+        bool3 = SD_size*j + h*position;
         bool4 = SD_size*i;
-        indx1 = ((bool3 <= x) & (x <= bool3 +h +10^6)) | ((bool3 +2*h <= x) & (x <= bool3 +3*h));
-        indy1 = (bool4 - h*hight <= y) & (y <= bool4 + h*hight);
-        indHuf = indHuf | (indx1&indy1);
-        indx2 = (bool3 <= x) & (x <= bool3 +3*h);
-        indy2 = (bool4 -2*h <= y) & (y <= bool4 -h);
-        indHuf = indHuf | (indx2&indy2);
+        %linker Streifen des Hufeisens
+        elements1 = false(numTri,1);
+        indx1 = (bool3 -tol <= x) & (x <= bool3 + width*h +tol) ;
+        indy1 = (bool4 - h*hight -tol <= y) & (y <= bool4 + h*hight +tol);
+        ind1 = indx1&indy1;
+        vertNum1 = find(ind1); % Knotennummern der Knoten mit hoehem Koeffizienten
+        elements1(sum(ismember(tri,vertNum1),2)==3) = true;
+
+        %rechter Streifen des Hufeisens
+        elements2 = false(numTri,1);
+        indx2 = (bool3 +2*width*h -tol <= x) & (x <= bool3 +3*width*h +tol);
+        indy2 = (bool4 - h*hight -tol <= y) & (y <= bool4 + h*hight +tol);
+        ind2 = indx2&indy2;
+        vertNum2 = find(ind2); % Knotennummern der Knoten mit hoehem Koeffizienten
+        elements2(sum(ismember(tri,vertNum2),2)==3) = true;
+
+        %Verbiundung zwischen Streifen
+        elements3 = false(numTri,1);
+        indx3 = (bool3 -tol <= x) & (x <= bool3 +3*width*h +tol);
+        indy3 = (bool4 -(width+hight)*h -tol <= y) & (y <= bool4 -h*hight +tol);
+        ind3 = indx3&indy3;
+        vertNum3 = find(ind3); % Knotennummern der Knoten mit hoehem Koeffizienten
+        elements3(sum(ismember(tri,vertNum3),2)==3) = true;
+
+
+        elements = elements | (elements1 | elements2 | elements3);
+        indHuf = indHuf | ind1 | ind2 | ind3;
     end
 end
 markedVertices = indSt | indHuf;
 
-
-%% Plotten des Gitters mit Kanal
-figure(2);
-scatter(vert(markedVertices,1),vert(markedVertices,2),[],"r") % Dirichletknoten markieren
+% %% Plotten des Gitters mit Kanal
+% figure(2);
+% scatter(vert(markedVertices,1),vert(markedVertices,2),[],"r") % Dirichletknoten markieren
 
 end
 
