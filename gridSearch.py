d58789d73233b6c83e7d6fc020cf74feb3f0bfe4
Ricu <vl1102@web.de>
2022-06-30 19:59:11 +0200

    Zwischenstand, randomsearch sollte laufen

--- gridSearch.py
+++ gridSearch.py
@@ -8,152 +8,167 @@ Created on Tue Jun 28 13:14:50 2022
 # loading the required modules
 import numpy as np
 import pandas as pd
-import keras
 import matplotlib.pyplot as plt
-import timeit
-from keras.models import Sequential
-from keras.layers import Dense
-from sklearn.model_selection import GridSearchCV
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import mean_squared_error, make_scorer, r2_score
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, log_loss
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-from keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping
 
 random_state = 0
 
 # Datensatz laden
-# train_data = pd.read_csv('C:/Users/Angelina/Documents/GitHub/WR-2-3-FFN-CLF/resources/train_data/2022-06-28-11-05-08-train_data_dump.csv', header = None)
-train_data = pd.read_csv('C:/Users/Valentin/Documents/10. Semester/WR2/Projekt 3 - FNN CLF/WR-2-3-FFN-CLF/resources/train_data/training_dataset_ver1.csv', header = None)
-coeff = train_data.iloc[:,:-1].values
-label = train_data.iloc[:,-1:].values
-
-#xtrain, xtest, ytrain, ytest=train_test_split(x, y, test_size=0.15)
+# train_data1000 = pd.read_csv('C:/Users/Angelina/Documents/GitHub/WR-2-3-FFN-CLF/resources/train_data/2022-06-28-11-05-08-train_data_dump.csv', header = None)
+train_data1000 = pd.read_csv('C:/Users/Valentin/Documents/10. Semester/WR2/Projekt 3 - FNN CLF/WR-2-3-FFN-CLF/resources/train_data/training_dataset_ver1.csv', header = None)
+train_data = train_data1000.loc[~np.any(train_data1000 == 1000,axis = 1)] 
+X = train_data.iloc[:,:-1].values
+y = train_data.iloc[:,-1:].values
 
 # Splitte Daten in Test und Trainingsdatensatz auf
-coeff_train, coeff_test, label_train, label_test = train_test_split(coeff,
-                                                                    label,
-                                                                    test_size=0.2)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Input skalieren
-sc_st = MinMaxScaler()
-sc_st.fit(coeff)
-coeff_train = sc_st.transform(coeff_train)
-coeff_test = sc_st.transform(coeff_test)
+normalizer = MinMaxScaler()
+normalizer.fit(X)
+X_train = normalizer.transform(X_train)
+X_test = normalizer.transform(X_test)
 
+metrics = [tf.keras.metrics.BinaryAccuracy(),
+           tf.keras.metrics.BinaryCrossentropy(),
+           tf.keras.metrics.AUC(),
+           tf.keras.metrics.Precision(),
+           tf.keras.metrics.Recall(),
+           tf.keras.metrics.TrueNegatives()
+           ]
 # Definiere Neuronales Netz
-def build_model(learning_rate,n_hidden_layers,layer_size,activation):
-    inputs = keras.Input(shape = (4800,), name = 'input_layer')
-    model = keras.Sequential(inputs)
-    # model = Sequential()
-    #for i in range(hp.Int("n_layers", 1, 3)):
+def build_model(learning_rate,n_hidden_layers,layer_size,activation,dropout_rate):
+    model = Sequential()
+    
     for i in range(n_hidden_layers):
-       # model.add(Dense(units=hp.Int(f"units_{i}", min_value=32, max_value=128, step=32),
-       #                        activation = 'sigmoid'
-        #    ))
-         model.add(Dense(units=layer_size,activation = activation))
+        model.add(tf.keras.layers.Dropout(dropout_rate, seed=random_state+i))
+        model.add(Dense(units=layer_size,activation = activation))
          
-    model.add(Dense(1, activation = activation))
+    # Output Layer
+    model.add(Dense(1, activation = 'sigmoid'))
         
-    model.compile(loss = keras.losses.MeanSquaredError(),
-                  optimizer = keras.optimizers.Adam(lr=learning_rate),
-                  metrics = ["accuracy"])
+    model.compile(loss = 'binary_crossentropy',
+                  optimizer = tf.keras.optimizers.Adam(lr=learning_rate),
+                  metrics = metrics)
     return model
 
 model = KerasClassifier(build_model, verbose = 0)
 
-# Zu testende Parameter des Modells
-#parameters = {
-# 'batch_size': [16, 32],
-# 'epochs': [100, 200],
-# 'learning_rate' : [0.001, 0.005],
-# 'layer_size' : [2],
-# 'output_shape' : [100]
-# }
-# Weitere parameters
-# 'activation' : ['sigmoid','relu']
-# 'data_percentage' : [1/3,1/2,1]
-# 'dropout'
-# 'patience'
-# 'min_delta'
-
-activation      = ['sigmoid','relu']
-batch_size      = [8,16,32,64]
-epochs          = [100,200]
-learning_rate   = [0.001,0.01,0.1]
-n_hidden_layers = [1,2,3,4]
-layer_size      = [100,500,1000]
-
-
-
+# Schnelle Testaparameter
 parameters = {
-    'activation' : activation,
-        
-    'batch_size': batch_size,
-    'epochs': epochs,
-    'learning_rate' : learning_rate,
-    'n_hidden_layers' : n_hidden_layers,
-    'layer_size' : layer_size
+    'activation' : ['sigmoid'],  
+    'batch_size': [32],
+    'epochs': [10],
+    'learning_rate' : [0.01],
+    'n_hidden_layers' : [1],
+    'layer_size' : [2,6,10],
+    'dropout_rate' : [0]
     }
 
+
+# Weitere parameters
+# 'data_percentage' : [1/3,1/2,1]
+# init = ['glorot_uniform', 'normal', 'uniform']
+
+# activation      = ['sigmoid','relu']
+# batch_size      = [8,16,32,64]
+# epochs          = [100,200,400]
+# learning_rate   = [0.001,0.01,0.1]
+# n_hidden_layers = [1,2,3,4]
+# layer_size      = [100,500,1000]
+# dropout_rate    = [0, 0.1, 0.2, 0.5]
+
+# parameters = {
+#     'activation' : activation,  
+#     'batch_size': batch_size,
+#     'epochs': epochs,
+#     'learning_rate' : learning_rate,
+#     'n_hidden_layers' : n_hidden_layers,
+#     'layer_size' : layer_size,
+#     'dropout_rate' : dropout_rate
+#     }
+
 # Definiere score-function
-score = make_scorer(mean_squared_error)
 cv = StratifiedKFold(n_splits = 4, shuffle = True, random_state = random_state)
 
-gridsearch = GridSearchCV(estimator = model,
-                          param_grid = parameters,
-                          scoring=score,
-                          cv=cv,
-                          #return_train_score=True,
-                          verbose = 4)
+def false_negatives_loss(y_true,y_pred):
+    # Berechnet den Anteil an FN/(FN+TP)
+    fn = sum(np.all([np.array(y_true) == 1,np.array(y_pred) == 0],0)) 
+    n_positives = sum(np.array(y_true) == 1)
+    return fn/n_positives
 
-callback = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=10)
+fn_loss = make_scorer(false_negatives_loss,greater_is_better = False)
+randomsearch = RandomizedSearchCV(estimator = model,
+                                  param_distributions = parameters,
+                                  n_iter = 50,
+                                  scoring={'accuracy':'accuracy',
+                                           'neg_log_loss':'neg_log_loss',
+                                           'f1' : 'f1',
+                                           'fn_loss': fn_loss},   
+                                  refit = 'accuracy',
+                                  cv=cv,
+                                  return_train_score=True,
+                                  verbose = 4)
+
+# callback = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=10)
+# callback = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=20)
 
 # Wende Grid Search auf Daten an
-start = timeit.timeit()
-gridsearch.fit(coeff_train, np.ravel(label_train), callbacks=[callback], validation_split=0.1, verbose = 0) 
-end = timeit.timeit()
+randomsearch.fit(X_train, y_train, verbose = 0) 
 
+result_df = pd.DataFrame.from_dict(randomsearch.cv_results_)
+    
+best_estim=randomsearch.best_estimator_
 
-best_estim=gridsearch.best_estimator_
-
-
-# Wende bestes Modell auf Daten an
-# best_estim ist bereits auf coeff_train gefittet
-# best_estim.fit(coeff_train, np.ravel(label_train), callbacks=[callback], validation_split=0.1)
 
 # Vorhersage
-labeltr_pred=best_estim.predict(coeff_train)
+y_test_pred=best_estim.predict(X_test)
+# print("Scores des besten Classifier auf den Testdaten:")
+# for m in metrics:
+#     m.update_state(y_test,y_test_pred)
+#     with tf.Session() as sess:
+#         m.result().eval()
+#     print(m.name + ": %.4f" % m.result().numpy())
 
-# Berechne Fehler
-mse = mean_squared_error(labeltr_pred,label_train)
 
-print('Benoetigte Zeit:',end - start)
-print('Best Parameters:',gridsearch.best_params_)
-print('Best Estimator:',best_estim)
 
-r2 = r2_score(labeltr_pred,label_train)
-print("Traindata: MSE: %.2f" % mse)
-print("Traindata: R2: %.2f" % r2)
-print("Traindata: ACC: %.2f" % accuracy_score(labeltr_pred,label_train))
 
-# Testen
-labelpred=best_estim.predict(coeff_test)
 
-# Berechne Fehler
-mse = mean_squared_error(label_test, labelpred)
-r2 = r2_score(label_test, labelpred)
-print("Testdata: MSE: %.2f" % mse)
-print("Testdata: R2: %.2f" % r2)
-print("Testdata: ACC: %.2f" % accuracy_score(labelpred,label_test))
 
-x_ax = range(len(label_test))
-plt.scatter(x_ax, label_test, s=5, color="blue", label="original")
-plt.plot(x_ax, labelpred, lw=0.8, color="red", label="predicted")
-plt.legend()
-plt.show()
+# # Berechne Fehler
+# mse = mean_squared_error(labeltr_pred,y_train)
+
+# print('Best Parameters:',randomsearch.best_params_)
+# print('Best Estimator:',best_estim)
+
+# r2 = r2_score(labeltr_pred,y_train)
+# print("Traindata: MSE: %.2f" % mse)
+# print("Traindata: R2 : %.2f" % r2)
+# print("Traindata: ACC: %.2f" % accuracy_score(labeltr_pred,y_train))
+
+# # Testen
+# labelpred=best_estim.predict(X_test)
+
+# # Berechne Fehler
+# mse = mean_squared_error(y_test, labelpred)
+# r2 = r2_score(y_test, labelpred)
+# print("Testdata: MSE: %.2f" % mse)
+# print("Testdata: R2: %.2f" % r2)
+# print("Testdata: ACC: %.2f" % accuracy_score(labelpred,y_test))
+
+# x_ax = range(len(y_test))
+# plt.scatter(x_ax, y_test, s=5, color="blue", label="original")
+# plt.plot(x_ax, labelpred, lw=0.8, color="red", label="predicted")
+# plt.legend()
+# plt.show()
 
